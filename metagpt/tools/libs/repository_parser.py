3a4699cb249ba3011029e018c7c0ee1cbdba264a
Joseph Call <jbcallv@gmail.com>
2025-02-08 17:07:39 -0500

    add tool for parsing github

---
+++ metagpt/tools/libs/repository_parser.py
@@ -0,0 +1,12 @@
+import subprocess
+from metagpt.tools.tool_registry import register_tool
+
+@register_tool(tags=["repository", "parsing", "parser", "github"], include_functions=["__init__", "clone_repository"])
+class RepositoryParser:
+    """
+    A repository parsing tool that clones a GitHub repository and parses relevant code components into memory
+    """
+
+    @staticmethod
+    def clone_repository(repo_url, target_dir: str = "."):
+        subprocess.run(['git', 'clone', repo_url, target_dir], check=True)
